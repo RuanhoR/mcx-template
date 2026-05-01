9bb5cabb7e0cae3086f12ac44ffd470ef44ac539
Ruanhor <3915264929@qq.com>
2026-05-01 14:54:00 +0800

    chore: change gitignore and test mbler new config

--- mbler.config.js
+++ mbler.config.js
@@ -1,18 +1,23 @@
+/**@ts-check */
 import { defineConfig } from "mbler"
 export default defineConfig({
   name: '@ruanhor/example',
   description: 'demo for mcx',
   mcVersion: '1.21.120',
-  version: '0.0.1',
+  version: '0.0.2',
   minify: false,
   script: {
     main: 'index.ts',
     ui: true,
     lang: 'mcx',
+    UseBeta: true
+  },
+  build: {
+    bundle: true
   },
   outdir: {
     resources: './dist/res',
     behavior: './dist/dep',
     dist: './dist.mcaddon',
-  },
+  }
 });
