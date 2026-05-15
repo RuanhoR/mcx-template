50282cdbc3c12aba5f32bf5b13cb886f655e173c
ruanhor <3915264929@qq.com>
2026-05-15 20:48:45 +0800

    fix: fix event regiser error

--- mbler.config.js
+++ mbler.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   name: '@ruanhor/example',
   description: 'demo for mcx',
   mcVersion: '1.21.120',
-  version: '0.0.2',
+  version: '0.0.1',
   minify: false,
   script: {
     main: 'index.ts',
