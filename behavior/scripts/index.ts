cee6b60ad6771cf11c30118a90729a0b9f94f8cb
ruanhor <3915264929@qq.com>
2026-07-01 18:15:41 +0800

    chore: dep update

--- behavior/scripts/index.ts
+++ behavior/scripts/index.ts
@@ -1,8 +1,5 @@
 import App from './app.mcx'
 import './component/ComponentTest.mcx'
 import { createApp } from '@mbler/mcx'
-import { system, world } from '@minecraft/server'
+import { world } from '@minecraft/server'
 createApp(App).mount(world)
-system.beforeEvents.startup.subscribe(event => {
-  event.dimensionRegistry.registerCustomDimension('custom_dis:test')
-})
